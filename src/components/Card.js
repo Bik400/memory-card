0ad9fe0a021d3db296c24d9e0dc6f85eca157356
Bik400 <biksingh30@gmail.com>
2022-04-27 15:34:22 -0700

    changed class name

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <div className="card-div" onClick={props.onSelectChar}>
             <img src={props.char.img} alt={props.char.name} id={props.char.id} className="char-img"></img>
-            <span className="classbro">{props.char.name}</span>
+            <span className="card-name">{props.char.name}</span>
         </div>
     )
 };
